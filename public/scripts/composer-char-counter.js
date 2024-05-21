e9cfbd8de46346b755223c1d7c975813af92a081
SorrenJ <soalexjao@gmail.com>
2024-05-21 00:57:27 -0700

    simple counter, number decreases as user types in textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -22,9 +22,16 @@ $(document).ready(function() {
       $("#tweet-text").on('keyup', function(event) {
       
         var charCount = $(this).val().length;
+        var setLimit = 140;
+        var charCounter = setLimit - charCount;
+
         // alert(`user wrote ${charCount} characters`);
         // Update the character count display
-        $('#char-counter').text(charCount);
+        $('#char-counter').text(charCounter);
+
+        if (charCounter < 0){
+        $('#char-counter').css( "color", "red");
+        }
     
       });
     
