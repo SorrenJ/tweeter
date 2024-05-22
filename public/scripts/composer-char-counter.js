e5435a824f6933ab4cfaac1804888582b9bb74ad
SorrenJ <soalexjao@gmail.com>
2024-05-22 01:22:30 -0700

    additional styling, placeholder tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,24 +1,6 @@
 $(document).ready(function() {
     // --- our code goes here ---
-    alert("document is ready");
   
-
-
-    $("#btn_test").on('click', function() {
-        alert("user pressed the button"); //The this keyword is a reference to the button
-      });
-      
-      $("#btn_test").on('click', () => {
-        alert("user pressed the button"); //The this keyword here refers to something else!
-      });
-
-
-
-      $("#char-counter").on('keypress', () => {
-        alert("user wrote something"); //The this keyword here refers to something else!
-      });
-
-
       $("#tweet-text").on('keyup', function(event) {
       
         var charCount = $(this).val().length;
